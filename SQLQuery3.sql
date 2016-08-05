185d48a4f6e71f905a3b328b1cbd4e2edc70719c
Ben Farmer <ben_farmer3@hotmail.com>
2016-08-05 22:33:11 +0100

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,15 @@
+﻿--select * from dbo.artist
+--where name like 'p%'
+
+--select * from dbo.artist
+--where name like 'm%'
+
+--select * from dbo.artist
+
+--insert into dbo.artist
+--(name)
+--values('Abba');
+
+--insert into dbo.album
+--(title,DateOfRelease,ArtistId,HighestUkChartPosition)
+--values ('xxxx',???,,,,)
